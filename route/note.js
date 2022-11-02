22c5171fc90d36f92f40108db915b6d1c8cb9a9d
Matthew Swineford <m.swineford@yahoo.com>
2022-11-02 15:43:48 -0400

    worked on note.js

--- route/note.js
+++ route/note.js
@@ -1,1 +1,25 @@
 const note = require("express").Router();
+const { writeToFile, readFromFile, readAndAppend } = require("../helpers/fsUtils");
+
+notes.get ("/", (req, res) =>
+    readFromFile("./db/db.json").then((data) =>
+        res.json(JSON.parse(data)))
+);
+notes.post("/", (req, res) => {
+    const {title, text} = req.body;
+    if (title && text) {
+        const newNote = { 
+            title, 
+            text, 
+            note_id: uuid(),
+        };
+        readAndAppend(newNote, "./db/db.json");
+        const response = {
+            status: "success",
+            body: newNotes,
+        };
+        res.json(response);
+    } else {
+        res.json("Error posting note.");
+    }
+});
